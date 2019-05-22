324b5b76191dc1fbbe9faca1560997aa1851e2f7
ankit50 <ankeetkharel@gmail.com>
2019-05-22 17:19:45 +0545

    basic class

---
+++ OOP/OOPS_1.py
@@ -0,0 +1,26 @@
+class Employee:
+    def __init__(self, firstName, lastName, age, salary):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.salary = salary
+
+    def sum(self):
+        if self.age > 28:
+            print("You can take hooneymoon package")
+        else:
+            print("you cannot take honeymoon package")
+
+    def empDetails(self):
+        print("FIRST NAME: {}".format(self.firstName))
+        print("LAST NAME: {}".format(self.lastName))
+        print("AGE: {}".format(self.age))
+        print("SALARY: {}".format(self.salary))
+
+
+employee = Employee("Ankit", "Kharel", 23, "100k")
+employee.empDetails()
+employee.sum()
+
+
+
