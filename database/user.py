3b905d9dba6cc2e9e88db33056a5dea35015fa13
ankit50 <ankeetkharel@gmail.com>
2019-05-30 22:33:54 +0545

    Edit data added

--- database/user.py
+++ database/user.py
@@ -30,6 +30,7 @@ def viewRecord():
 		print("Id value not Found In Database.Please Enter A Valid Id Number.")
 	finally:
 		cursor.close()
+		conn.close()
 
 #Inserting Record To the Database
 def insertRecord():
@@ -50,11 +51,42 @@ def insertRecord():
 		print("Error!!!!")
 	finally:
 		cursor.close()
+		conn.close()
+def editRecord():
+	try:
+		cursor, conn = connection()
+		ID = int(input("Enter The Student ID:"))
+		query = 'SELECT *FROM personal_records WHERE "ID" = %s'
+		cursor.execute(query, (ID,))
+		details = cursor.fetchall()
+		for row in details:
+			print("---------------------------------------")
+			print("ID:{}".format(row[ 0 ]))
+			print("Student Name:{} {}".format(row[ 1 ], row[ 2 ]))
+			print("Phone:{}".format(row[ 3 ]))
+			print("Gender:{}".format(row[ 4 ]))
+			print("Address:{}".format(row[ 5 ]))
+			Id = int(input("Enter Id:"))
+			fname = input("Enter First Name:")
+			lname = input("Enter Last Name:")
+			phone = int(input("Enter Phone Number:"))
+			gen = input("Enter Gender:")
+			add = input("Enter Address:")
+			recordsToUpdate = (Id, fname, lname, phone, gen, add, ID)
+			editQuery = 'UPDATE personal_records SET "ID"=%s, "firstName"=%s, "lastName"=%s, "Phone"=%s, "Gender"=%s, "Address"=%s WHERE "ID"=%s'
+			cursor.execute(editQuery, recordsToUpdate )
+			print("Record Updated Succesfully!!!")
+			conn.commit()
+	except Exception:
+		print("Error")
+	finally:
+		cursor.close()
+		conn.close()
 
 if __name__=="__main__":
 	while True:
 		print("---------------------------------------")
-		print("1.View Record\n2.Add Record\n3.Exit")
+		print("1.View Record\n2.Add Record\n3.Edit\n4.Exit")
 		Choice = int(input("Enter Your Choice:"))
 		if Choice == 1:
 			#Fetching Row From Database Using ID
@@ -64,7 +96,6 @@ if __name__=="__main__":
 			insertRecord()
 
 		elif Choice == 3:
-			cursor, conn = connection()
-			cursor.close()
-			conn.close()
+			editRecord()
+		elif Choice == 4:
 			break
